8725f9a262d530970319073bc25b25c74db72d1b
al364891 <44838961+al364891@users.noreply.github.com>
2019-11-26 13:32:19 +0100

    asset1

---
+++ Estrategia/Assets/scripts/mouseManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseManager : MonoBehaviour
+{
+
+    Transform seletedUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GameObject ourHitObject = hitInfo.collider.transform.gameObject;
+
+                Debug.Log("Raycast hit:" + hitInfo.transform.position);
+
+                //Debug.Log("ourHitObject:" + ourHitObject.transform.position);
+
+                if (hitInfo.transform.CompareTag("Player"))
+                {
+                    SelectUnit(hitInfo.transform);
+                }
+                else if (hitInfo.transform.CompareTag("ground"))
+                {
+                    MoveUnit(hitInfo.transform);
+                }
+                //Debug.Log("unidad elegida:" + seletedUnit.position);
+
+
+            }
+        }
+
+    }
+
+    public void MoveUnit(Transform cube)
+    {
+        seletedUnit.position = new Vector3(cube.position.x -5, seletedUnit.position.y, cube.position.z -5);
+    }
+
+    public void SelectUnit(Transform unit)
+    {
+
+        if (seletedUnit != null)
+        {
+            seletedUnit.Find("circulo").gameObject.SetActive(false);
+        }
+
+        seletedUnit = unit;
+        seletedUnit.Find("circulo").gameObject.SetActive(true);
+    }
+
+}
---
+++ Estrategia/Assets/scripts/unidades_parametros.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unidades_parametros : MonoBehaviour
+{
+    public int ataque;
+    public int vision;
+    public int rangoDeAtaque;
+    public int vida;
+    public int coste;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
