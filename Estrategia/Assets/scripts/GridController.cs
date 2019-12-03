2ea69e1dcecf3a300da2c9c029a0035ebbee57f8
alexcercos <44820718+alexcercos@users.noreply.github.com>
2019-12-03 01:20:32 +0100

    unos cuantos cambios
    Cambios realizados:
    -he rehecho el escenario para que las distancias sean unitarias y positivas (la primera esta en x=0, z=0)
    
    -he guardado prefabs de cada tipo de casilla Y HE PUESTO LA NIEBLA DENTRO DESACTIVADA, de forma que se puede activar desde el mismo gameobject en un solo script (la niebla es el child 0)
    
    -En escenario he creado un script "GridController" que guarda las referencias de las casillas en una matriz ordenada. Ahi se podran crear matrices para calcular la visibilidad o incluso un pathfinding sencillo

---
+++ Estrategia/Assets/scripts/GridController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    int rows = 17;
+    int cols = 12;
+
+    private GameObject[,] gridObjects;
+
+    //crear 
+
+
+    void Awake()
+    {
+        //En awake se guardan las referencias de gameObjects de casillas en una matriz, con su posicion como indice
+
+        gridObjects = new GameObject[rows, cols];
+
+        for (int i = 0; i<transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+
+            //Local position para que solo dependa de la posicion interna
+
+            //NO SE DEBE MOVER LAS CASILLAS DE DENTRO DEL OBJETO ESCENARIO (deben ir de x,z = 0,0 a 16,11)
+
+            int x = (int)child.localPosition.x;
+            int y = (int)child.localPosition.z;
+
+            gridObjects[x, y] = child.gameObject;
+            
+            /*
+            if (gridObjects[x, y] == null)
+            {
+                Debug.Log("done " + x + " " + y);
+                gridObjects[x, y] = child.gameObject;
+            } else
+            {
+                Debug.Log("repeated " + x + " " + y);
+            }*/
+        }
+    }
+
+    //con otras funciones se pueden crear mapas de influencia o 
+    //hacer busqueda de profundidad para comprobar la visibilidad del jugador Y del enemigo (por separado)
+    //tambien se puede hacer un pathfiding sencillo con esa bfs
+}
