99e603e710722b4123143af625fcdd4a912fcd40
alexcercos <44820718+alexcercos@users.noreply.github.com>
2019-12-12 04:23:14 +0100

    pruebas

---
+++ Estrategia/Assets/scripts/GOAP/AIAgent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SwordGC.AI.Goap;
+
+public class AIAgent : GoapAgent
+{
+    public override void Awake()
+    {
+        base.Awake();
+
+        //create goals
+
+
+        //create actions
+    }
+    
+}
